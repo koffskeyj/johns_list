82dd27242a8794b6a63e8072669808fca3e3de5f
koffskeyj <koffskeyj@gmail.com>
2016-06-26 15:14:48 -0400

    Sorting working

--- craigs_list/views.py
+++ craigs_list/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import TemplateView, ListView, DetailView, View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.template import RequestContext
 from django.core.urlresolvers import reverse_lazy
 from craigs_list.models import Category, SubCategory, Profile, Listing
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -36,6 +38,7 @@ class ListingCreateView(LoginRequiredMixin, CreateView):
         return super(ListingCreateView, self).form_valid(form)
 
 class UserListingView(LoginRequiredMixin, ListView):
+    template_name = "user_listing.html"
 
     def get_queryset(self):
         return Listing.objects.filter(user=self.request.user)
@@ -59,10 +62,62 @@ class UserCityListingView(ListView):
 class AnonChooseCityView(FormView):
     template_name = "anon_choose_city.html"
     form_class = CityForm
+    success_url = "/anon_choose_city/anon_listings"
+
+    # def form_valid(self, form):
+        # city_choice = form.save(commit=False)
+        # city_choice.city = Profile.city
+        # return super(AnonChooseCityView, self).form_valid(form)
+
+    # def get_city(request):
+        # if request.method == "POST":
+            # form = CityForm(request.POST)
+            # if form.is_valid():
+                # clean_city = form.cleaned_data["city"]
+        # else:
+            # form = CityForm()
+        # return render_to_response("craigs_list/anon_listing_list.html", {"form" : form}, context_instance=RequestContext(request))
+
+class AnonListingsView(ListView):
+    template_name = "craigs_list/anon_listing_list.html"
 
 
+    def get_queryset(self):
+        form = CityForm()
+        if form.city == "Greenville":
+            return Listing.objects.filter(city="Greenville")
+    # def get_queryset(self):
+        # Case(
+            # When(city="Greenville", then=Listing.objects.filter(city="Greenville")),
+            # When(city="Columbia", then=Listing.objects.filter(city="Columbia")),
+            # When(city="Spartanburg", then=Listing.objects.filter(city="Spartanburg")))
 
-class ListingDetailedView(LoginRequiredMixin, DetailView):
+
+class SortByCreatedView(ListView):
+    template_name = "sort_by_created.html"
+
+    def get_queryset(self, **kwargs):
+        category_id = self.kwargs.get('pk', None)
+        return Listing.objects.filter(listing_subcategory_id=category_id).order_by("-created")
+
+
+class SortByHighestView(ListView):
+    template_name = "sort_by_highest.html"
+
+    def get_queryset(self, **kwargs):
+        category_id = self.kwargs.get('pk', None)
+        return Listing.objects.filter(listing_subcategory_id=category_id).order_by("-value")
+
+
+class SortByLowestView(ListView):
+    template_name = "sort_by_lowest.html"
+
+    def get_queryset(self, **kwargs):
+        category_id = self.kwargs.get('pk', None)
+        return Listing.objects.filter(listing_subcategory_id=category_id).order_by("value")
+
+
+class ListingDetailedView(DetailView):
     model = Listing
 
 
